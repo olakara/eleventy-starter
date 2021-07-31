b8a5a68f709b5917886451b07c35fe982e11929f
Abdel Raoof <olakara@gmail.com>
2021-07-31 09:09:07 +0400

    initial commit with bare minimum function

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = (eleventyConfig) => {
+
+
+  eleventyConfig.addPassthroughCopy("./src/assets/");
+
+  eleventyConfig.addWatchTarget("./src/assets/styles.css");
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
